a99b7501191546fc4ec050509a7de36265fb801f
Naseem Badran <naseem.b@circ.zone>
2025-07-26 01:06:08 +0300

    fixxx

--- Miass-Extension/src/main.tsx
+++ Miass-Extension/src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { HashRouter } from "react-router-dom"; // 👈 ADD THIS
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    {" "}
+    {/* 👈 WRAP APP IN THIS */}
+    <App />
+  </HashRouter>
+);
