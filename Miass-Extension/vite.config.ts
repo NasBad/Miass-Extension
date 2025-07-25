b359c1171c201e7d54236a99132732c6df3a9e19
Naseem Badran <naseem.b@circ.zone>
2025-07-26 01:01:24 +0300

    fix

--- Miass-Extension/vite.config.ts
+++ Miass-Extension/vite.config.ts
@@ -5,10 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/Miass-Extension/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
   plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
